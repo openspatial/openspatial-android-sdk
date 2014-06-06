19fe69724129586a2653fc47b7e0fe322ebef6cf
Rahul Iyer <rni@enimai.com>
2014-06-06 15:22:07 -0700

    Update example app to include multi device support
    This diff updates the app to work with to multi device support and
    uses the NodPointer service to display multiple pointers to track
    each pointer.
    
    Change-Id: I6c740a334638f7b2614e044ecf8bc5ff97fe6aaa
    Reviewed-on: https://r.enimai.com/1627
    Reviewed-by: Rahul Iyer <rni@enimai.com>
    Tested-by: Rahul Iyer <rni@enimai.com>

--- src/com/example/openspatial/MainScreenActivity.java
+++ src/com/example/openspatial/MainScreenActivity.java
@@ -16,101 +16,163 @@
 
 package com.example.openspatial;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import com.nod_labs.pointer.PointerService;
 import net.openspatial.*;
 
+import java.util.Set;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class MainScreenActivity extends Activity {
-    OpenSpatialService mService;
+    OpenSpatialService mOpenSpatialService;
+    PointerService mPointerService;
 
     private static String TAG = MainScreenActivity.class.getSimpleName();
+    private static final int RADIUS = 20;
+    private static final int mWhiteColor = Color.WHITE;
+    private static final int mRedColor = Color.RED;
 
-    ServiceConnection mServiceConnection = new ServiceConnection() {
+    private ServiceConnection mOpenSpatialServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            mService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
+            mOpenSpatialService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
 
-            try {
-                mService.registerForPointerEvents(null, new OpenSpatialEvent.EventListener() {
-                    @Override
-                    public void onEventReceived(OpenSpatialEvent event) {
-                        PointerEvent pEvent = (PointerEvent)event;
-                        Log.d(TAG, "Received PointerEvent of type " + pEvent.pointerEventType +
-                        " with x: " + pEvent.x + " and y: " + pEvent.y);
-                    }
-                });
-            } catch (OpenSpatialException e) {
-                Log.e(TAG, "Error registering for PointerEvent " + e);
-            }
-
-            try {
-                mService.registerForButtonEvents(null, new OpenSpatialEvent.EventListener() {
-                    @Override
-                    public void onEventReceived(OpenSpatialEvent event) {
-                        ButtonEvent bEvent = (ButtonEvent)event;
-
-                        Log.d(TAG, "Received ButtonEvent of type " + bEvent.buttonEventType);
-                    }
-                });
-            } catch (OpenSpatialException e) {
-                Log.e(TAG, "Error registering for PointerEvent " + e);
-            }
-
-            try {
-                mService.registerForRotationEvents(null, new OpenSpatialEvent.EventListener() {
-                    @Override
-                    public void onEventReceived(OpenSpatialEvent event) {
-                        RotationEvent rotationEvent = (RotationEvent) event;
-
-                        // Commenting because this is to noisy
-                        //Log.d(TAG, "Got rotationEvent with value " + rotationEvent.getQuaternion());
-                    }
-                });
-            } catch (OpenSpatialException e) {
-                Log.e(TAG, "Error registering for PointerEvent " + e);
-            }
-
-            OpenSpatialEvent.EventListener gestureEventListener = new OpenSpatialEvent.EventListener() {
+            mOpenSpatialService.getConnectedDevices(new OpenSpatialService.OpenSpatialServiceCallback() {
                 @Override
-                public void onEventReceived(OpenSpatialEvent event) {
-                    GestureEvent gestureEvent = (GestureEvent)event;
-                    Log.d(TAG, "Got GestureEvent of type " + gestureEvent.gestureEventType +
-                            " with magnitude " + gestureEvent.magnitude);
-                }
-            };
+                public void deviceListUpdated(Set<BluetoothDevice> bluetoothDevices) {
+                    Log.d(TAG, "Num connected devices = " + bluetoothDevices.size());
+                    for (BluetoothDevice device : bluetoothDevices) {
+                        mPointerService.addPointer(device.getAddress(),
+                                RADIUS,
+                                Color.alpha(mWhiteColor),
+                                Color.red(mWhiteColor),
+                                Color.green(mWhiteColor),
+                                Color.blue(mWhiteColor),
+                                device.getName());
 
-            try {
-                mService.registerForGestureEvents(null, GestureEvent.GestureEventType.SWIPE_UP, gestureEventListener);
-                mService.registerForGestureEvents(null, GestureEvent.GestureEventType.SWIPE_DOWN, gestureEventListener);
-                mService.registerForGestureEvents(null, GestureEvent.GestureEventType.SWIPE_LEFT, gestureEventListener);
-                mService.registerForGestureEvents(null, GestureEvent.GestureEventType.SWIPE_RIGHT, gestureEventListener);
-            } catch (OpenSpatialException e) {
-                Log.e(TAG, "Error registering for GestureEvent " + e);
-            }
+                        Log.d(TAG, "Registering " + device.getName());
+                        registerForEvents(device);
+                    }
+                }
+            });
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            mService = null;
+            mOpenSpatialService = null;
         }
     };
+
+    private ServiceConnection mPointerServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mPointerService = ((PointerService.PointerServiceBinder)service).getService();
+            bindService(new Intent(MainScreenActivity.this,
+                    OpenSpatialService.class),
+                    mOpenSpatialServiceConnection,
+                    BIND_AUTO_CREATE);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mPointerService = null;
+        }
+    };
+
+    private void registerForEvents(final BluetoothDevice device) {
+        try {
+            mOpenSpatialService.registerForPointerEvents(device, new OpenSpatialEvent.EventListener() {
+                @Override
+                public void onEventReceived(OpenSpatialEvent event) {
+                    PointerEvent pEvent = (PointerEvent) event;
+                    mPointerService.updatePointerPosition(device.getAddress(), pEvent.x, pEvent.y);
+                }
+            });
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Error registering for PointerEvent " + e);
+        }
+
+        try {
+            mOpenSpatialService.registerForButtonEvents(device, new OpenSpatialEvent.EventListener() {
+                @Override
+                public void onEventReceived(OpenSpatialEvent event) {
+                    ButtonEvent bEvent = (ButtonEvent) event;
+
+                    Log.d(TAG, device.getName() + ": received ButtonEvent of type " + bEvent.buttonEventType);
+
+                    if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH2_DOWN) {
+                        mPointerService.updatePointerColor(device.getAddress(),
+                                Color.alpha(mRedColor),
+                                Color.red(mRedColor),
+                                Color.green(mRedColor),
+                                Color.blue(mRedColor));
+                        mPointerService.updatePointerRadius(device.getAddress(), 2 * RADIUS);
+                    } else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH2_UP) {
+                        mPointerService.updatePointerColor(device.getAddress(),
+                                Color.alpha(mWhiteColor),
+                                Color.red(mWhiteColor),
+                                Color.green(mWhiteColor),
+                                Color.blue(mWhiteColor));
+                        mPointerService.updatePointerRadius(device.getAddress(), RADIUS);
+                    }
+                }
+            });
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Error registering for PointerEvent " + e);
+        }
+
+        try {
+            mOpenSpatialService.registerForRotationEvents(device, new OpenSpatialEvent.EventListener() {
+                @Override
+                public void onEventReceived(OpenSpatialEvent event) {
+                    RotationEvent rotationEvent = (RotationEvent) event;
+                }
+            });
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Error registering for PointerEvent " + e);
+        }
+
+        OpenSpatialEvent.EventListener gestureEventListener = new OpenSpatialEvent.EventListener() {
+            @Override
+            public void onEventReceived(OpenSpatialEvent event) {
+                GestureEvent gestureEvent = (GestureEvent)event;
+                Log.d(TAG, device.getName() + ": got GestureEvent of type " + gestureEvent.gestureEventType +
+                        " with magnitude " + gestureEvent.magnitude);
+            }
+        };
+
+        try {
+            mOpenSpatialService.registerForGestureEvents(device, GestureEvent.GestureEventType.SWIPE_UP, gestureEventListener);
+            mOpenSpatialService.registerForGestureEvents(device, GestureEvent.GestureEventType.SWIPE_DOWN, gestureEventListener);
+            mOpenSpatialService.registerForGestureEvents(device, GestureEvent.GestureEventType.SWIPE_LEFT, gestureEventListener);
+            mOpenSpatialService.registerForGestureEvents(device, GestureEvent.GestureEventType.SWIPE_RIGHT, gestureEventListener);
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Error registering for GestureEvent " + e);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
 
-        bindService(new Intent(this, OpenSpatialService.class), mServiceConnection, BIND_AUTO_CREATE);
+        bindService(new Intent(this, PointerService.class), mPointerServiceConnection, BIND_AUTO_CREATE);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        unbindService(mServiceConnection);
+        unbindService(mOpenSpatialServiceConnection);
+        unbindService(mPointerServiceConnection);
     }
 }
