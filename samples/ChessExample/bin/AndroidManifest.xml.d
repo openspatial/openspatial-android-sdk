d1449ebb1aa1b883f006cb4a10d21bc06d82b604
Rahul Iyer <rni@enimai.com>
2014-07-30 18:26:27 -0700

    Remove bin and gen directories that were accidentally added
    Change-Id: If682f0a9ebb7c54ca608031647bf022381e23d4a
    Reviewed-on: https://r.enimai.com/2343
    Reviewed-by: Srinivasan Sridharan <srini@enimai.com>
    Tested-by: Rahul Iyer <rni@enimai.com>

--- samples/ChessExample/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/bin/AndroidManifest.xml : \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/AndroidManifest.xml \
--- samples/ChessExample/bin/ChessExample-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/bin/ChessExample-debug-unaligned.apk : \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/bin/ChessExample.ap_ \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/bin/classes.dex \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/src/com/example/example.iml \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/libs/modelloader.jar \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/libs/OpenSpatialSDKv0.6.jar \
-/Volumes/repo_disk/enimai/openspatial/android-sdk/samples/ChessExample/libs/cardboard.jar \
