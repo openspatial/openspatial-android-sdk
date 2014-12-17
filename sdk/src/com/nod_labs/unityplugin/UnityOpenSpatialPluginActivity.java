42c0dc6ba67bc6fe7834304cfaf040d868fb9549
Srinivasan Sridharan <srini@enimai.com>
2014-12-16 17:08:56 -0800

    Implement APIs needed by Unity's NodControllerInterface
    Subscribe to events (pointer, gesture etc.) and get data.
    Cache the data in a map as they come out as Unity can poll
    faster than events come out.
    
    Reset relative pointer x, y data upon read lest it drift
    forever.
    
    Change-Id: I73b96cda2686764799918e106dc0b802f7321185
    Reviewed-on: https://r.enimai.com/3076
    Tested-by: Srinivasan Sridharan <srini@enimai.com>
    Reviewed-by: Peter Williams <peter@nod-labs.com>

--- sdk/src/com/nod_labs/unityplugin/UnityOpenSpatialPluginActivity.java
+++ sdk/src/com/nod_labs/unityplugin/UnityOpenSpatialPluginActivity.java
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.primitives.Ints;
 import com.unity3d.player.UnityPlayerNativeActivity;
 
 import net.openspatial.ButtonEvent;
@@ -20,123 +23,111 @@ import net.openspatial.OpenSpatialService;
 import net.openspatial.PointerEvent;
 import net.openspatial.Pose6DEvent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class UnityOpenSpatialPluginActivity extends UnityPlayerNativeActivity {
 
-    OpenSpatialService mOpenSpatialService;
+    private static OpenSpatialService mOpenSpatialService;
     private static String TAG = UnityOpenSpatialPluginActivity.class.getSimpleName();
 
-    private static Map<String, BluetoothDevice> addressMap = new HashMap<String, BluetoothDevice>();
-    private static Map<BluetoothDevice, float[]> rotationMap = new HashMap<BluetoothDevice, float[]>();
-    private static Map<BluetoothDevice, boolean[]> buttonMap = new HashMap<BluetoothDevice, boolean[]>();
-    private static Map<BluetoothDevice, int[]> pointerMap = new HashMap<BluetoothDevice, int[]>();
-    private static ArrayList<String> addressList= new ArrayList<String>();
+    private static int mNextId = 0;
+    private static BiMap<Integer, BluetoothDevice> mDeviceIdMap = HashBiMap.create();
 
-    public static int getNumDevices(){
-        return addressList.size();
-    }
+    private static Map<Integer, float[]> mRotationMap = new HashMap<Integer, float[]>();
+    private static Map<Integer, int[]> mPointerMap = new HashMap<Integer, int[]>();
+    private static Map<Integer, Integer> mButtonMap = new HashMap<Integer, Integer>();
+    private static Map<Integer, Integer> mGestureMap = new HashMap<Integer, Integer>();
 
-    public static String getDeviceAddress(int deviceIndex){
-        return "TEST STRING HERE";
-    }
-
-    // Call from Unity to set a button's values to false
-    public static void unityButtonCallback(String deviceAddress, int buttonType){
-        if(buttonType == 0){
-//          buttonMap.get(addressMap.get(deviceAddress))[0] = buttonMap.get(addressMap.get(deviceAddress))[1] = false;
-        }
-        else{
-//          buttonMap.get(addressMap.get(deviceAddress))[2] = buttonMap.get(addressMap.get(deviceAddress))[3] = false;
-        }
-    }
-
-    // Returns a boolean array of form [touch0, touch1, touch2]
-    public static boolean[] getButtonData(String deviceAddress){
-        return buttonMap.get(addressMap.get(deviceAddress));
-    }
-
-    // Returns an integer array of form [x, y]
-    public static int[] getPointerData(String deviceAddress){
-        return pointerMap.get(addressMap.get(deviceAddress));
-    }
-
-    // Returns a float array of form [pitch, roll, yaw]
-    public static float[] getRotationData(String deviceAddress){
-        return rotationMap.get(addressMap.get(deviceAddress));
-    }
-
-    private ServiceConnection mOpenSpatialServiceConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.d(TAG, "Service Connected");
-            mOpenSpatialService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
-            mOpenSpatialService.initialize(TAG, new OpenSpatialService.OpenSpatialServiceCallback() {
-
+    private static OpenSpatialService.OpenSpatialServiceCallback mOpenSpatialServiceCallback =
+            new OpenSpatialService.OpenSpatialServiceCallback() {
                 @Override
                 public void deviceConnected(BluetoothDevice device) {
-                    Log.d(TAG, "Registering Device" + device);
+                    Log.d(TAG, "Connected to device: " + device.getAddress());
 
-                    rotationMap.put(device, new float[3]);
-                    buttonMap.put(device, new boolean[3]);
-                    pointerMap.put(device, new int[2]);
-                    addressMap.put(device.getAddress(), device);
-                    addressList.add(device.getAddress());
+                    // FIXME: not ideal for generating id, use hash of address
+                    int deviceId = mNextId++;
+                    mDeviceIdMap.put(deviceId, device);
 
-                    registerForEvents(device);
+                    mRotationMap.put(deviceId, new float[3]);
+                    mPointerMap.put(deviceId, new int[2]);
+                    mButtonMap.put(deviceId, new Integer(0));
+                    mGestureMap.put(deviceId, new Integer(0));
                 }
 
                 @Override
                 public void deviceDisconnected(BluetoothDevice device) {
-                    rotationMap.remove(device);
-                    buttonMap.remove(device);
-                    pointerMap.remove(device);
-                    addressMap.remove(device.getAddress());
-                    addressList.remove(device.getAddress());
+                    Integer deviceId = mDeviceIdMap.inverse().get(device);
+                    if (deviceId != null) {
+                        mRotationMap.remove(deviceId);
+                        mPointerMap.remove(deviceId);
+                        mButtonMap.remove(deviceId);
+                        mGestureMap.remove(deviceId);
+                    }
                 }
 
                 @Override
                 public void buttonEventRegistrationResult(BluetoothDevice device, int status) {
                 }
+
                 @Override
                 public void pointerEventRegistrationResult(BluetoothDevice device, int status) {
                 }
+
                 @Override
                 public void pose6DEventRegistrationResult(BluetoothDevice device, int status) {
                 }
+
                 @Override
                 public void gestureEventRegistrationResult(BluetoothDevice device, int status) {
                 }
-                @Override
-                public void motion6DEventRegistrationResult(BluetoothDevice device, int i) {
-                }
-            });
 
+                @Override
+                public void motion6DEventRegistrationResult(BluetoothDevice device, int status) {
+                }
+            };
+
+    private static ServiceConnection mOpenSpatialServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "Service Connected");
+            mOpenSpatialService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
+            mOpenSpatialService.initialize(TAG, mOpenSpatialServiceCallback);
             mOpenSpatialService.getConnectedDevices();
+            mNextId = 0;
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+            mOpenSpatialServiceCallback = null;
             mOpenSpatialService = null;
+            mNextId = 0;
         }
     };
 
-    private void registerForEvents(final BluetoothDevice device) {
-        try {
-            mOpenSpatialService.registerForPointerEvents(device, new OpenSpatialEvent.EventListener() {
-                @Override
-                public void onEventReceived(OpenSpatialEvent event) {
-                    PointerEvent pEvent = (PointerEvent) event;
-                    pointerMap.get(device)[0] = pEvent.x;
-                    pointerMap.get(device)[1] = pEvent.y;
-                }
-            });
-        } catch (OpenSpatialException e) {
-            Log.e(TAG, "Error registering for PointerEvent " + e);
+    public static int[] getDeviceIds() {
+        return Ints.toArray(mDeviceIdMap.keySet());
+    }
+
+    public static int getNumDevices(){
+        return mDeviceIdMap.size();
+    }
+
+    public static String getDeviceAddress(int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "No device with id=" + deviceId + " found");
+            return null;
+        }
+        return device.getAddress();
+    }
+
+    public static boolean registerForButtonEvents(final int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Register for Button events failed: No device with id=" + deviceId);
+            return false;
         }
 
         try {
@@ -145,68 +136,199 @@ public class UnityOpenSpatialPluginActivity extends UnityPlayerNativeActivity {
                 public void onEventReceived(OpenSpatialEvent event) {
                     ButtonEvent bEvent = (ButtonEvent) event;
                     Log.d(TAG, device.getName() + ": received ButtonEvent of type " + bEvent.buttonEventType);
-                    if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH0_DOWN) {
-                        buttonMap.get(device)[0] = true;
-                    } else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH0_UP) {
-                        buttonMap.get(device)[0] = false;
-                    }else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH1_DOWN) {
-                        buttonMap.get(device)[1] = true;
-                    } else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH1_UP) {
-                        buttonMap.get(device)[1] = false;
+
+                    int buttons = mButtonMap.get(deviceId);
+                    switch (bEvent.buttonEventType) {
+                        case TOUCH0_DOWN:   buttons |=  (1 << 0); break;
+                        case TOUCH0_UP:     buttons &= ~(1 << 0); break;
+                        case TOUCH1_DOWN:   buttons |=  (1 << 1); break;
+                        case TOUCH1_UP:     buttons &= ~(1 << 1); break;
+                        case TOUCH2_DOWN:   buttons |=  (1 << 2); break;
+                        case TOUCH2_UP:     buttons &= ~(1 << 2); break;
+                        case TACTILE0_DOWN: buttons |=  (1 << 3); break;
+                        case TACTILE0_UP:   buttons &= ~(1 << 4); break;
+                        case TACTILE1_DOWN: buttons |=  (1 << 5); break;
+                        case TACTILE1_UP:   buttons &= ~(1 << 5); break;
                     }
-                    else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH2_DOWN){
-                        buttonMap.get(device)[2] = true;
-                    }
-                    else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH2_UP){
-                        buttonMap.get(device)[2] = false;
-                    }
-                    Log.d(TAG, "buttons: " + buttonMap.get(device)[0] + "  " + buttonMap.get(device)[1]);
+
+                    mButtonMap.put(deviceId, buttons);
+                    Log.d(TAG, "buttons: " + Integer.toHexString(buttons));
                 }
             });
         } catch (OpenSpatialException e) {
-            Log.e(TAG, "Error registering for Button Event " + e);
+            Log.e(TAG, "Register for Button events failed: " + e.getMessage());
+            return false;
         }
+
+        return true;
+    }
+
+    public static boolean registerForPointerEvents(final int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Register for Pointer events failed: No device with id=" + deviceId);
+            return false;
+        }
+
+        try {
+            mOpenSpatialService.registerForPointerEvents(device, new OpenSpatialEvent.EventListener() {
+                @Override
+                public void onEventReceived(OpenSpatialEvent event) {
+                    PointerEvent pEvent = (PointerEvent) event;
+                    int[] pos2d = mPointerMap.get(deviceId);
+                    pos2d[0] = pEvent.x;
+                    pos2d[1] = pEvent.y;
+                }
+            });
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Register for Pointer events failed: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean registerForPose6DEvents(final int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Register for Pose6D events failed: No device with id=" + deviceId);
+            return false;
+        }
+
         try {
             mOpenSpatialService.registerForPose6DEvents(device, new OpenSpatialEvent.EventListener() {
                 @Override
                 public void onEventReceived(OpenSpatialEvent event) {
                     Pose6DEvent pose6DEvent = (Pose6DEvent) event;
-                    float[] pry = rotationMap.get(device);
-                    pry[0] = pose6DEvent.pitch;
-                    pry[1] = pose6DEvent.roll;
-                    pry[2] = pose6DEvent.yaw;
+                    float[] pose = mRotationMap.get(deviceId);
+                    pose[0] = pose6DEvent.pitch;
+                    pose[1] = pose6DEvent.roll;
+                    pose[2] = pose6DEvent.yaw;
                 }
             });
         } catch (OpenSpatialException e) {
-            Log.e(TAG, "Error registering for Pose6DEvent " + e);
+            Log.e(TAG, "Register for Pose6D events failed: " + e.getMessage());
+            return false;
         }
 
-
-        OpenSpatialEvent.EventListener gestureEventListener = new OpenSpatialEvent.EventListener() {
-            @Override
-            public void onEventReceived(OpenSpatialEvent event) {
-                GestureEvent gesture = (GestureEvent)event;
-                Log.d(TAG, device.getName() + ": got GestureEvent of type " + gesture.gestureEventType +
-                        " with magnitude " + gesture.magnitude);
-            }
-        };
-
-        try {
-            mOpenSpatialService.registerForGestureEvents(device, gestureEventListener);
-        } catch (OpenSpatialException e) {
-            Log.e(TAG, "Error registering for GestureEvent " + e);
-        }
+        return true;
     }
 
-    private void unregisterFromEvents(BluetoothDevice device) {
+    public static boolean registerForGestureEvents(final int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Register for Gesture events failed: No device with id=" + deviceId);
+            return false;
+        }
+
+        try {
+            mOpenSpatialService.registerForGestureEvents(device, new OpenSpatialEvent.EventListener() {
+                @Override
+                public void onEventReceived(OpenSpatialEvent event) {
+                    GestureEvent gesture = (GestureEvent)event;
+                    Log.d(TAG, device.getName() + ": got GestureEvent of type " + gesture.gestureEventType +
+                            " with magnitude " + gesture.magnitude);
+                    mGestureMap.put(deviceId, gesture.gestureEventType.ordinal());
+                }
+            });
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Register for Gesture events failed: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean unregisterFromButtonEvents(int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Unregister from Button events failed: No device with id=" + deviceId);
+            return false;
+        }
+        try {
+            mOpenSpatialService.unRegisterForButtonEvents(device);
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Unregister from Button events failed: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean unregisterFromPointerEvents(int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Unregister from Pointer events failed: No device with id=" + deviceId);
+            return false;
+        }
         try {
             mOpenSpatialService.unRegisterForPointerEvents(device);
-            mOpenSpatialService.unRegisterForButtonEvents(device);
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Unregister from Pointer events failed: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean unregisterFromPose6DEvents(int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Unregister from Pose6D events failed: No device with id=" + deviceId);
+            return false;
+        }
+        try {
             mOpenSpatialService.unRegisterForPose6DEvents(device);
+        } catch (OpenSpatialException e) {
+            Log.e(TAG, "Unregister from Pose6D events failed: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean unregisterFromGestureEvents(int deviceId) {
+        final BluetoothDevice device = mDeviceIdMap.get(deviceId);
+        if (device == null) {
+            Log.e(TAG, "Unregister from Gesture events failed: No device with id=" + deviceId);
+            return false;
+        }
+        try {
             mOpenSpatialService.unRegisterForGestureEvents(device);
         } catch (OpenSpatialException e) {
-            Log.e(TAG, "Could not unregister from events: " + e);
+            Log.e(TAG, "Unregister from Gesture events failed: " + e.getMessage());
+            return false;
         }
+
+        return true;
+    }
+
+    // Returns a boolean array of form [touch0, touch1, touch2]
+    public static int getButtonData(int deviceId) {
+        return mButtonMap.get(deviceId);
+    }
+
+    // Returns an integer array of form [x, y]
+    public static int[] getPointerData(int deviceId) {
+        int[] pos2d =  mPointerMap.get(deviceId);
+
+        int[] ret = new int[pos2d.length];
+        System.arraycopy(pos2d, 0, ret, 0, pos2d.length);
+
+        // reset on read
+        pos2d[0] = 0;
+        pos2d[1] = 0;
+
+        return ret;
+    }
+
+    // Returns a float array of form [pitch, roll, yaw]
+    public static float[] getRotationData(int deviceId) {
+        return mRotationMap.get(deviceId);
+    }
+
+    public static int getGestureData(int deviceId) {
+        return mGestureMap.get(deviceId);
     }
 
     @Override
